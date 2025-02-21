35b1109071558e8191a1836fae7b6a45bc79db81
HanGyeolee <46367614+HanGyeolee@users.noreply.github.com>
2025-02-22 00:23:47 +0900

    대충 배포

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import {Suspense, useEffect, useRef} from 'react';
-import { Route, Routes, useNavigate, redirect, useLocation, Navigate } from 'react-router-dom';
+import {Suspense} from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import { Home, APW } from './pages';
 import {NotFound} from './components/ui/NotFound';
 
